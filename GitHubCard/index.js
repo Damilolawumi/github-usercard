4525ad38d763d9c23753e68de6808daf6770bb90
damilola.olawumi <damilola_olawumi@yahoo.com>
2019-09-05 19:01:50 +0100

    successfully created my profile

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -3,10 +3,22 @@
            https://api.github.com/users/<your name>
            https://api.github.com/users/Damilolawumi
 */
-axios.get('https://api.github.com/users/Damilolawumi')
-  .then((response) => {
-    console.log(response.data)
-  })
+
+function fetchUser(username) {
+  axios.get(`https://api.github.com/users/${username}`)
+    .then((response) => {
+
+      let component = userComponentCreator(response.data)
+      let cards = document.querySelector('.cards');
+
+      cards.appendChild(component)
+      for(let i = 0; i <= followersArray.length; i++)
+    })
+   
+
+}
+
+
 
 /* Step 2: Inspect and study the data coming back, this is YOUR 
    github info! You will need to understand the structure of this 
@@ -29,7 +41,7 @@ axios.get('https://api.github.com/users/Damilolawumi')
           user, and adding that card to the DOM.
 */
 
-const followersArray = [];
+const followersArray = ['tetondan', 'dustinmyers', 'justsml', 'luishrd', 'bigknell'];
 
 /* Step 3: Create a function that accepts a single object as its only argument,
           Using DOM methods and properties, create a component that will return the following DOM element:
@@ -61,31 +73,54 @@ const followersArray = [];
   bigknell
 */
 
-const bioData = function (userInfo) {
-  const firstDiv = document.createElement('div');
-  firstDiv.classList.add('.card');
+const userComponentCreator = function (userInfo) {
+  const cardDiv = document.createElement('div');
+  cardDiv.classList.add('.card');
 
   const Image = document.createElement('img');
   Image.src = userInfo.avatar_url;
 
-  const secondDiv = document.createElement('div');
-  secondDiv.classList.add('.card-info')
+  const cardInfo = document.createElement('div');
+  cardInfo.classList.add('.card-info')
 
   const myH3 = document.createElement('h3')
   myH3.classList.add('.name')
   myH3.textContent = userInfo.name;
 
-  const firstParagraph = document.createElement('p');
-  firstParagraph.classList.add('.username');
-  firstParagraph.textContent = userInfo.login;
+  const userNameParagraph = document.createElement('p');
+  userNameParagraph.classList.add('.username');
+  userNameParagraph.textContent = userInfo.login;
 
-  const secondParagraph = document.createElement('p');
-  secondParagraph.textContent = userInfo.location;
+  const locationParagraph = document.createElement('p');
+  locationParagraph.textContent = userInfo.location;
 
-  const thirdParagraph = document.createElement('p');
-  thirdParagraph.textContent = 'Profile:'
+  const profileParagraph = document.createElement('p');
+  profileParagraph.textContent = 'Profile:'
 
-const anchor = document.createElement('a');
-anchor.href = userInfo.html_url;
+  const anchor = document.createElement('a');
+  anchor.href = userInfo.html_url;
+  anchor.textContent = userInfo.html_url
 
+  const followersParagraph = document.createElement('p')
+  followersParagraph.textContent = `Followers: ${userInfo.followers}`
+
+  const followingParagraph = document.createElement('p');
+  followingParagraph.textContent = `Following: ${userInfo.following}`
+
+  const bioParagraph = document.createElement('p');
+  bioParagraph.textContent = `Bio: ${userInfo.bio}`
+
+
+  profileParagraph.appendChild(anchor);
+  cardInfo.appendChild(myH3);
+  cardInfo.appendChild(userNameParagraph);
+  cardInfo.appendChild(locationParagraph);
+  cardInfo.appendChild(profileParagraph);
+  cardInfo.appendChild(followersParagraph);
+  cardInfo.appendChild(followingParagraph);
+  cardInfo.appendChild(bioParagraph);
+  cardDiv.appendChild(Image);
+  cardDiv.appendChild(cardInfo);
+
+  return cardDiv
 }
